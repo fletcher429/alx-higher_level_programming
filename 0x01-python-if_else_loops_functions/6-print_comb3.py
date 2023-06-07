3f1fee0202453e9b2f32a95e6f5c17d4de7e6f0b
David <davifletcher2003@gmail.com>
2023-06-07 22:10:30 +0300

    hello_julien

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for n in range(0, 9):
     for m in range(n, 10):
         if n == m:
             continue
-        print("{}{}".format(n, m), end=", " 
+        print("{}{}".format(n, m), end=", "
               if not (n == 8 and m == 9) else "\n")
