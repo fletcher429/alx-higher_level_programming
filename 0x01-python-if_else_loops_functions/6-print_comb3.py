daad02bd6fb0d4bb73cf9d87eac74db63cf71010
David <davifletcher2003@gmail.com>
2023-06-07 14:07:32 +0300

    fix it

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for n in range(0, 10):
     for m in range(n, 10):
         print("{}{:d}".format(n, m), end=", " if n < 9 else "\n")
     if n < 9:
-        print("\b\b", end=", ")
+        end =", "
