340530cf18ae0db766700bb055cc9d75bb2100f1
David <davifletcher2003@gmail.com>
2023-06-07 13:02:08 +0300

    fixed char

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for n in range(0, 99):
+for n in range(0, 100):
     if n < 10:
         print(f"{0}{n:d}", end=", ")
     else:
-        print("{}".format(n), end=", "if n < 98 else "")
+        print("{}".format(n), end=", "if n < 99 else "")
