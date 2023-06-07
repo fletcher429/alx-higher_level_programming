7e58ad7537caf972286dd949a1939e2f2d79531f
David <davifletcher2003@gmail.com>
2023-06-08 00:06:35 +0300

    convert lowercase to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    store = ""
+    a = ord('a')
+    z = ord('z')
+    for char in str:
+        if a <= ord(char) <= z:
+            upper = chr(ord(char) - 32)
+            store += upper
+        else:
+            store += char
+    print(store)
---
+++ 0x01-python-if_else_loops_functions/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+uppercase = __import__('test').uppercase
+
+uppercase("fAck You")
+uppercase("Best School 98 Battery street")
