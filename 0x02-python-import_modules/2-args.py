faa1414e9ea59df1e10aa6064af6605a6ecb68c3
David <davifletcher2003@gmail.com>
2023-06-09 22:35:09 +0300

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     le = len(argv) - 1
     if le == 0:
         print(f"{le} arguments.")
+    elif le == 1:
+        print(f"{le} argument:")
     else:
-        print(f"{le} arguments:")
+        print(f"{le} argument:")
     for i in range(1, le + 1):
         print(f"{i}: {argv[i]}")
