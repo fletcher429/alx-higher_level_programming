54a1bc329649278b13583e70560f8103acd25617
David <davifletcher2003@gmail.com>
2023-06-09 22:28:40 +0300

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    le = len(argv) - 1
+    if le == 0:
+        print(f"{le} arguments.")
+    else:
+        print(f"{le} arguments:")
+    for i in range(1, le + 1):
+        print(f"{i}: {argv[i]}")
