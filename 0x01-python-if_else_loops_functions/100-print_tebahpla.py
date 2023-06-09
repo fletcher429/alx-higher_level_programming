ef240b6ebc5147d7bd0d5159e388c421be0c1544
David <davifletcher2003@gmail.com>
2023-06-09 12:57:08 +0300

    telabaplpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(25, -1, -1):
     if num % 2 == 0:
-        print(format(chr(num % 26 + 65)), end="")
+        print("{0}".format(chr(num % 26 + 65)), end="")
     else:
-        print(format(chr(num % 26 + 97)), end="")
+        print("{0}".format(chr(num % 26 + 97)), end="")
