7b5f5de8673c51a9324473ea6eb54473247aadea
David <davifletcher2003@gmail.com>
2023-06-12 14:44:08 +0300

    okey

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-from test import add, mul, div, sub
-from sys import argv
+
 if __name__ == "__main__":
-    result = 0
-    a = int(argv[1])
-    b = int(argv[3])
-    oper = argv[2]
-    for i in range(1, len(argv)):
-        if oper == '+':
-            result = add(a, b)
-        elif oper == '-':
-            result = sub(a, b)
-        elif oper == '*':
-            result = mul(a, b)
-        elif oper == '/':
-            result = div(a, b)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-    print("{} {} {} = {}".format(a, oper, b, result))
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
