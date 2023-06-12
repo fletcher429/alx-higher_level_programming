d9197528db986db033e8de86c5d87d7fe3fdf72c
David <davifletcher2003@gmail.com>
2023-06-12 09:14:11 +0300

    Pycode Fixed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,9 @@
 
 from sys import argv
 
-sum = 0
-for i in range(1, len(argv)):
-    sum += int(argv[i])
+if __name__ == "__main__":
+
+    sum = 0
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
     print(sum)
