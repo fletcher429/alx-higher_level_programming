47206b316049a0d6c65cc50f0d4533c055970169
David <davifletcher2003@gmail.com>
2023-06-14 22:29:07 +0300

    move the append outside the inner loop

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,5 +6,5 @@ def square_matrix_simple(matrix=[]):
         n_row = []
         for num in row:
             n_row.append(num ** 2)
-            new_matrix.append(n_row)
+        new_matrix.append(n_row)
     return new_matrix
