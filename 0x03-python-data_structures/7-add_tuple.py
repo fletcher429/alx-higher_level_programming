731bbe03cc768ebe3751182841892a4c5a12cabe
david <davifletcher20032gmail.com>
2023-08-21 14:49:28 +0300

    fixed (0,0)

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
- 
+
+    tuple_a = tuple_a[:2] + (0, 0)
+    tuple_b = tuple_b[:2] + (0, 0)
+
     sum_tuple_a = tuple_a[0] + tuple_b[0]
     sum_tuple_b = tuple_a[1] + tuple_b[1]
 
