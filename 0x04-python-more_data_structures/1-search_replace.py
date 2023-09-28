bc13097f5bb1f0b370c831e1c981083885874bf4
David <davifletcher2003@gmail.com>
2023-09-28 14:02:58 +0300

    pycodestyle_edited

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,14 +7,3 @@ def search_replace(my_list, search, replace):
         else:
             new_list.append(x)
     return new_list
-
-
-"""""
-Write a function that replaces all occurrences of an element by another in a new list.
-
-Prototype: def search_replace(my_list, search, replace):
-my_list is the initial list
-search is the element to replace in the list
-replace is the new element
-You are not allowed to import any module
-"""
