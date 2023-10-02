f074aed72b34e046d3219b5a661d4132db12b1f0
David <davifletcher2003@gmail.com>
2023-10-02 19:11:05 +0300

    Print rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -64,7 +64,9 @@ class Rectangle:
         if self.height == 0 or self.width == 0:
             return 0
         return 2 * (self.width + self.height)
-
+    """
+        Define the __str__ to print the rectangle
+    """
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ""
