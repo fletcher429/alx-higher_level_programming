cce5121456f77d5442a34d621b3585451e29e677
David <davifletcher2003@gmail.com>
2023-10-10 00:22:31 +0300

    find area - empty

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Define an empty class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Define public instance method keep it empty
+    """
+
+    def area(self):
+        raise NotImplementedError("area() is not implemented")
