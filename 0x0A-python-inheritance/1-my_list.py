91fa46f53e20f0a3000e15a90913b6fb7860bc32
David <davifletcher2003@gmail.com>
2023-10-09 22:31:51 +0300

    inherit base list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,12 +2,16 @@
 """
     Define a class that inherits from list as base
 """
+
+
 class MyList(list):
-    
+    """
+    Define a function that prints the sorted list
+    """
+
     def print_sorted(self):
         sort_list = sorted(self)
-
+        """
+            Print the sorted list
+        """
         print(sort_list)
-
-
-
