9c7106a6c2486f15fe79e7ef0db439851de24747
David <davifletcher2003@gmail.com>
2023-10-12 02:38:23 +0300

    json represantation

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,13 +3,15 @@
     write a function that append to a file and returns number of characters
     written
 """
+
+
 def append_write(filename="", text=""):
     """
-        Using with  to open and safely closing the file
+    Using with  to open and safely closing the file
     """
-    with open(filename, 'a', encoding="utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         """
-            Define a var that redas the file
+        Define a var that redas the file
         """
         data = f.write(f"{text}")
     """
