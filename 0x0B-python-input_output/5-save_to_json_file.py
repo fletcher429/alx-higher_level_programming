afe8b1a06326e42d136371d4b25afe9cbfaededc
David <davifletcher2003@gmail.com>
2023-10-12 03:01:57 +0300

    python to json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,16 +3,19 @@
     import json module
 """
 import json
+
 """
     Define a function that writes to a json file
 """
+
+
 def save_to_json_file(my_obj, filename):
     """
-        use with to safely open and close the file
-        write to json file to
+    use with to safely open and close the file
+    write to json file to
     """
-    with open(filename, 'w') as json_file:
+    with open(filename, "w") as json_file:
         """
-            use json.dump() to write and returns py
+        use json.dump() to write and returns py
         """
         json.dump(my_obj, json_file)
