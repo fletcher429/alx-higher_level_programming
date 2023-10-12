d5205b1541fba21945b6bfc860f6571b919e1237
David <davifletcher2003@gmail.com>
2023-10-12 03:26:15 +0300

    json decode

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -10,5 +10,11 @@ import json
 
 
 def load_from_json_file(filename):
+    """
+    use the with safely open and close the json file
+    """
     with open(filename) as json_file:
+        """
+        returns json decoded file
+        """
         return json.load(json_file)
