031b2ebbe66ae03cd0a9875295a6e8133c81282e
David <davifletcher2003@gmail.com>
2023-10-17 13:58:19 +0300

    correct the doc

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,31 +2,33 @@
 """
     Define a class called a base
 """
+
+
 class Base:
     """
-        inititilize __nb_objects to zero
+    inititilize __nb_objects to zero
     """
-    __nb_objects  = 0
+
+    __nb_objects = 0
     """
         init the attributes
     """
 
-    def __init__(self, id =None):
+    def __init__(self, id=None):
         """
-            check for the conditions
+        check for the conditions
         """
         """
             args(id) - the id
         """
         if id is not None:
-            self.id  = id
+            self.id = id
         else:
             """
-                increement the __nb_objects by 1
+            increement the __nb_objects by 1
             """
             Base.__nb_objects += 1
             """
                 update the id with __nb_objects
             """
-            self.id  = Base.__nb_objects
-
+            self.id = Base.__nb_objects
