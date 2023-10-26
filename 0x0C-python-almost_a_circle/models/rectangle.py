a446923d04c4d1272feb030c0b02ff09897e67fd
David <davifletcher2003@gmail.com>
2023-10-26 03:05:02 +0300

    added basic validations

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,97 +2,111 @@
 """
     import the Base module
 """
-from .base import Base
-
-"""
-    define a class named Rectangle
-"""
+from .base import Base  # Import the Base class from the base module
 
 
 class Rectangle(Base):
-    """
-    init the class
-    """
+    """Define a Rectangle class that inherits from the Base class."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        inherits from the base class
+        Initialize a Rectangle instance with its attributes.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle (default is 0).
+            y (int, optional): The y-coordinate of the rectangle (default is 0).
+            id (int, optional): The unique identifier for the rectangle (default is None).
         """
-        super().__init__(id)
+        super().__init__(id)  # Call the constructor of the base class with id
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-    """
-        use the getter proptery to get the attributes of the class - width
-    """
-
     @property
     def width(self):
+        """Get the width of the rectangle."""
         return self.__width
-
-    """
-        use the setter property to manipulates the width attribute
-    """
 
     @width.setter
     def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        Args:
+            value (int): The new width value.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an int")
+        if value <= 0:
+            raise ValueError("width must be greater than 0")
         self.__width = value
-
-    """
-         use the getter proptery to get the attributes of the class - height
-    """
 
     @property
     def height(self):
+        """Get the height of the rectangle."""
         return self.__height
-
-    """
-         use the setter property to manipulates the height attribute
-    """
 
     @height.setter
     def height(self, value):
+        """
+        Set the height of the rectangle.
+
+        Args:
+            value (int): The new height value.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an int")
-
-    """
-        use the getter proptery to get the attributes of the class - x
-    """
+        if value <= 0:
+            raise ValueError("height must be greater than 0")
+        self.__height = value
 
     @property
     def x(self):
+        """Get the x-coordinate of the rectangle."""
         return self.__x
-
-    """
-         use the setter property to manipulates the x attribute
-    """
 
     @x.setter
     def x(self, value):
+        """
+        Set the x-coordinate of the rectangle.
+
+        Args:
+            value (int): The new x-coordinate value.
+
+        Raises:
+            ValueError: If value is less than 0.
+        """
         if value < 0:
-            raise ValueError("x must be greater than or equals to 0")
+            raise ValueError("x must be greater than or equal to 0")
         self.__x = value
 
-    """
-        use the getter property to get the y attribute
-    """
-
     @property
-    def y(
-        self,
-    ):
+    def y(self):
+        """Get the y-coordinate of the rectangle."""
         return self.__y
-
-    """
-        use the setter attribute to set the attributes of the y attribute
-    """
 
     @y.setter
     def y(self, value):
+        """
+        Set the y-coordinate of the rectangle.
+
+        Args:
+            value (int): The new y-coordinate value.
+
+        Raises:
+            ValueError: If value is less than 0.
+        """
         if value < 0:
-            raise ValueError("y must be greater than or equals to 0")
+            raise ValueError("y must be greater than or equal to 0")
         self.__y = value
