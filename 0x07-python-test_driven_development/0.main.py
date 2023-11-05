f14cddc75cd06f1c987157aa3ef5086a0bc68413
David <davifletcher2003@gmail.com>
2023-11-05 15:02:06 +0300

    update repo

--- 0x07-python-test_driven_development/0.main.py
+++ 0x07-python-test_driven_development/0.main.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-add_integer = __import__("0-add_integer").add_integer
+matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(0, 10))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
+matrix = [
+    [9, 15, 18],
+    [12, 21, 42]
+]
+print(matrix_divided(matrix, 0))
+print(matrix)
