7638b82abb02eb41e61fcb10922113e2676ab7d3
David <davifletcher2003@gmail.com>
2023-11-05 15:07:46 +0300

    PEP 8 corrected

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,6 +2,8 @@
 """"
 A function that divides all elements of a matrix
 """
+
+
 def matrix_divided(matrix, div):
     len_row = len(matrix[0])
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
@@ -13,15 +15,15 @@ def matrix_divided(matrix, div):
     # initilize a new matrix to store the results
     new_matrix = []
 
-    #use a for loop to iterate through the row
+    # use a for loop to iterate through the row
     for row in matrix:
         # checks if each row is of the same size
         if len(row) != len_row:
             raise TypeError("Each row of the matrix must have the same size")
 
-        #intilize a empty to store the elements in a row
+        # intilize a empty to store the elements in a row
         new_row = []
-        #iterate through each element in a row
+        # iterate through each element in a row
         for element in row:
             # divide each element by specified div
             result = element / div
