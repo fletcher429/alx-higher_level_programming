9d3444a64c1ea61d71799ea79f9e66c3869235fb
David <davifletcher2003@gmail.com>
2023-11-07 22:00:47 +0300

    rec tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,7 +88,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise ValueError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
-import unittest
+"""
+import the rectangle module
+"""
 from models.rectangle import Rectangle
+"""
+imports the unittest module
+"""
+import unittest
 
+"""
+define a class to run the test
+"""
 class TestRectangle(unittest.TestCase):
+    """
+    define the test constructor of the
+    """
     def test_constructor(self):
         # Test the constructor
         rect = Rectangle(10, 20, 5, 5, 1)
@@ -11,3 +23,16 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rect.height, 20)
         self.assertEqual(rect.x, 5)
         self.assertEqual(rect.y, 5)
+    def test_instantation(self):
+        rec = Rectangle(1, 2)
+        self.assertIsInstance(rec, Rectangle)
+
+        rec2 = Rectangle(1, 2, 3)
+        self.assertIsInstance(rec2, Rectangle)
+
+
+        rec3 = Rectangle(1, 2, 3, 4)
+        self.assertIsInstance(rec3, Rectangle)
+    def test_no_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle()
