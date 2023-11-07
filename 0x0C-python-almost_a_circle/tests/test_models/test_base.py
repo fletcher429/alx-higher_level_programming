2cae9e1739fe8a21d9fa6baa59e2315a3ffc760d
David <davifletcher2003@gmail.com>
2023-11-07 21:32:38 +0300

    more test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -17,6 +17,18 @@ class test_base(unittest.TestCase):
     def test_constructor(self):
         b1 = Base(1)
         self.assertEqual(b1.id, 1)
+    def test_con_with_str(self):
+        b1 = Base("hello")
+        self.assertEqual(b1.id, "hello")
+    def test_con_inc(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+    def test_incremental(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b2.id, b1.id + 1)
 """
 Checks if the test is run as the main program
 """
