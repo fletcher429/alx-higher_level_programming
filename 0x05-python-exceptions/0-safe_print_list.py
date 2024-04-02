5054f636754c7701ae96a6c1bee61bf1a25b992f
David Ooko <davidomondiooko03@gmail.com>
2024-04-02 08:53:28 -0700

    try and except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,18 @@
 # /usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     """
-    Tries to print the interger in the List
+    ARGS - My_list[] where the items to printed are on
+    x - number of elements in the list
     """
     succes_print = 0
     for i in range(x):
+        """
+        use exception to make sure the correct output is printed  
+        """
         try:
             print("{}".format(my_list[i]), end="")
             succes_print += 1
+
         except IndexError:
             break
     print("")
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    Args:
+    my_list - the list to printed
+    x - is the number of items in the list
+    success_print - keeps track of int that are printed sucessfully
+    """
+    succes_print = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            succes_print += 1
+        except (TypeError, ValueError):
+            continue
+    print("")
+    return succes_print
