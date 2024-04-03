d51668edf3bda1cbfa856f8c5c40d1e484d37291
David Ooko <davidomondiooko03@gmail.com>
2024-04-03 11:57:17 -0700

    size attritubte which is made private

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+An empty class of a square
+"""
+
+
+class Square:
+    """
+        The class reprsesents a square object
+    """
+    def __init__(self, size):
+        """
+        Private instance of size
+        """
+        self.__size = size
