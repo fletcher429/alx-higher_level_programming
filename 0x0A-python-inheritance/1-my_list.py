7087fd637189fd633eb5a0168d178c1a8052b3da
David Ooko <davidomondiooko03@gmail.com>
2024-04-15 10:14:56 -0700

    try

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,25 +1,4 @@
 #!/usr/bin/python3
 """
-class MyList that inherits from list
+contains the MyList class
 """
-
-
-class MyList(list):
-    """
-    inti the class
-    """
-    def __init__(self):
-        super().__inti__()
-    """
-    define a public instance method
-    """
-
-    def print_sorted(self):
-        """
-        sorts the list in ascending order
-        """
-        result = sorted(self)
-        """
-        prints the list
-        """
-        print(result)
