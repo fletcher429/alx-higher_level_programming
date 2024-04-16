afdf5e033c5b9e9c501bfe99ce0aa3172650450a
David Ooko <davidomondiooko03@gmail.com>
2024-04-16 08:03:40 -0700

    gettatr

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""define a class student"""
+
+
+class Student:
+    """intilize the class with th attr"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    """retrieve a dictionary represantation of a student"""
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return vars(self)
+        else:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
