7c1f1d8e77133476da19a526490f9a4832e599d8
David Ooko <davidomondiooko03@gmail.com>
2024-04-16 04:59:00 -0700

    documentation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+defines a function that writes to the file
+"""
 def write_file(filename="", text=""):
+    """
+    using with open to effectively close the file after use to avoid the leaks
+    """
     with open(filename, 'w', encoding='utf-8') as file:
+        """
+        return 
+        """
         return (file.write(text))
