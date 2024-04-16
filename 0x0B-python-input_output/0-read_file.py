ef01e220390677d5476a5f58ef2fc3ea7fc34caf
David Ooko <davidomondiooko03@gmail.com>
2024-04-16 04:53:53 -0700

    documentatiom

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+    Function to open and read a file
+    Args:
+        filename - the filename
+"""
+
+
 def read_file(filename=""):
+    """
+    Use the with top open file
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         f = f.read()
         print(f)
