814b9b954bce0fdb6f06015949987f1698bb8802
David Ooko <davidomondiooko03@gmail.com>
2024-04-16 05:03:59 -0700

    pycodestyle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,6 +10,6 @@ def write_file(filename="", text=""):
     """
     with open(filename, 'w', encoding='utf-8') as file:
         """
-        return 
+        return
         """
         return (file.write(text))
