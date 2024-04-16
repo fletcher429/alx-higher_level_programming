485a8b016629f995e13e74a6b75cd34a0e555800
David Ooko <davidomondiooko03@gmail.com>
2024-04-16 05:40:19 -0700

    to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+importing the json modules
+"""
+import json
+
+"""
+converts python object to json string
+"""
+
+
+def to_json_string(my_obj):
+    return (json.dumps(my_obj))
