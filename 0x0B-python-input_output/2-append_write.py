a7f08a40cc9ec5ffb9c850f82a479fff1cdd4da2
David Ooko <davidomondiooko03@gmail.com>
2024-04-16 05:07:18 -0700

    append_file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+function that append text at the EOF and returns the number of characters
+"""
+
+
 def append_write(filename="", text=""):
+    """
+    using the with function to open the file
+    """
     with open(filename, 'a', encoding='utf-8') as file:
         file.write(text)
         return (len(text))
